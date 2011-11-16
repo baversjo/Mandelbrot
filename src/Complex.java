b382143d85e67f48151c8ee619880e4b7c98572d
Johan Baversjo <baversjo@gmail.com>
2011-11-16 11:15:48 +0100

    complex class

---
+++ src/Complex.java
@@ -0,0 +1,29 @@
+
+public class Complex {
+	private double re,im;
+	public Complex(double re, double im){
+		this.re = re;
+		this.im = im;
+	}
+	public double getRe(){
+		return re;
+	}
+	public double getIm(){
+		return im;
+	}
+
+	/** Tar reda på talets absolutbelopp i kvadrat */
+	public double getAbs2(){
+		return 0.0; //TODO: implement
+	}
+
+	/** Adderar det komplexa talet c till detta tal */
+	public void add(Complex c){
+		//TODO: implement
+	}
+
+	/** Multiplicerar detta tal med det komplexa talet c */
+	public void mul(Complex c){
+		//TODO: implement
+	}
+}
