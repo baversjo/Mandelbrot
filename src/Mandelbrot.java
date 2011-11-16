be1163d8a44f64538c308bff56db04667b7211cf
Adam Lyrén <adam.h.lyren@gmail.com>
2011-11-16 11:09:22 +0100

    Added first command cases

--- src/Mandelbrot.java
+++ src/Mandelbrot.java
@@ -1,3 +1,4 @@
+import se.lth.cs.ptdc.fractal.MandelbrotGUI;
 
 public class Mandelbrot {
 
@@ -6,7 +7,24 @@ public class Mandelbrot {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		MandelbrotGUI gui = new MandelbrotGUI();
+		while (true) {
+			switch (gui.getCommand()) {
+				case MandelbrotGUI.RENDER:
+					break;
+				case MandelbrotGUI.ZOOM:
+					break;
+				case MandelbrotGUI.RESET:
+					break;
+				case MandelbrotGUI.QUIT:
+					break;
+				default:
+					System.err.println("Command not yet implented: NR(" + gui.getCommand() + ")");
+					break;
+			}
+			
+		}
 	}
 
 }
