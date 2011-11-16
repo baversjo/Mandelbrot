fce0257c4ae6568b7006a4133c78a4a027f79723
Adam Lyrén <adam.h.lyren@gmail.com>
2011-11-16 11:47:50 +0100

    Added working render for test subject

--- src/Generator.java
+++ src/Generator.java
@@ -1,10 +1,38 @@
+import java.awt.Color;
 import se.lth.cs.ptdc.fractal.MandelbrotGUI;
 
 public class Generator {
 	public Generator() { }
 	
 	public void render(MandelbrotGUI gui) {
+		gui.disableInput();
+		int width = gui.getWidth();
+		int height = gui.getHeight();
+		Complex[][] complex = mesh(gui.getMinimumReal(), gui.getMaximumReal(),
+								   gui.getMinimumImag(), gui.getMaximumImag(),
+								   width, height);
 		
+		Color[][] picture = new Color[height][width];
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				
+				if (complex[y][x].getAbs2() > 1) {
+					picture[y][x] = Color.white;
+				} else if (complex[y][x].getRe() >= 0 && complex[y][x].getIm() >= 0) {
+					picture[y][x] = Color.red;
+				} else if (complex[y][x].getRe() < 0 && complex[y][x].getIm() >= 0) {
+					picture[y][x] = Color.blue;
+				} else if (complex[y][x].getRe() >= 0 && complex[y][x].getIm() < 0) {
+					picture[y][x] = Color.green;
+				} else if (complex[y][x].getRe() < 0 && complex[y][x].getIm() < 0) {
+					picture[y][x] = Color.yellow;
+				}
+				
+			}
+		}
+		
+		gui.putData(picture, 1, 1);
+		gui.enableInput();
 	}
 	
 	private Complex[][] mesh(double minRe, double maxRe, double minIm, double maxIm, int width, int height) {
--- src/Mandelbrot.java
+++ src/Mandelbrot.java
@@ -7,23 +7,33 @@ public class Mandelbrot {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		boolean rendered = false;
 		MandelbrotGUI gui = new MandelbrotGUI();
+		Generator g = new Generator();
 		while (true) {
 			switch (gui.getCommand()) {
 				case MandelbrotGUI.RENDER:
+					gui.clearPlane();
+					g.render(gui);
+					rendered = true;
 					break;
 				case MandelbrotGUI.ZOOM:
+					if (rendered) {
+						g.render(gui);
+					}
 					break;
 				case MandelbrotGUI.RESET:
+					gui.resetPlane();
+					gui.clearPlane();
+					rendered = false;
 					break;
 				case MandelbrotGUI.QUIT:
+					System.exit(0);
 					break;
 				default:
 					System.err.println("Command not yet implented: NR(" + gui.getCommand() + ")");
 					break;
-			}
-			
+			}			
 		}
 	} 
 
